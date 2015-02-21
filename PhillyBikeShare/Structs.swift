5c1b961cbc18d89a0aa6516083d11222e4ab3c47
Mike Stanziano <mikejs@aweber.com>
2015-02-21 12:42:22 -0500

    Update Structs.swift and Rack struct to map to Core Data

--- PhillyBikeShare/Structs.swift
+++ PhillyBikeShare/Structs.swift
@@ -6,7 +6,29 @@
 //  Copyright (c) 2015 Team Awesome. All rights reserved.
 //
 
-struct Rack {
+//! Protocol which declares that a API Struct can be mapped to Core Data.
+protocol CoreDataMappable
+{
+    var coreDataEntityName: String { get }
+
+    func mapToCoreDataObject(inContext managedContext: NSManagedObjectContext) -> NSManagedObject
+}
+
+//! Fuction to add a Core Data friendly object to Core Data.
+func addObjectToCoreData(object: CoreDataMappable)
+{
+    let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+    let managedContext = appDelegate.managedObjectContext!
+
+    object.mapToCoreDataObject(inContext: managedContext)
+
+    var error: NSError?
+    if !managedContext.save(&error) {
+        println("Could not save \(error), \(error?.userInfo)")
+    }
+}
+
+struct Rack: CoreDataMappable {
     let rackId: String
     let lat: String
     let lng: String
@@ -14,7 +36,8 @@ struct Rack {
     let maxBikes: Int
     let availBikes: Int
 
-    static func decode(json: [String : AnyObject]) -> Rack? {
+    //! Decode from provided JSON dictionary.
+    func decode(json: [String : AnyObject]) -> Rack? {
         if let rackId = json["_id"] as? String {
             if let lat = json["lat"] as? String {
                 if let lng = json["lng"] as? String {
@@ -32,4 +55,25 @@ struct Rack {
         return .None
     }
 
+    var coreDataEntityName: String {
+        return "Rack"
+    }
+
+    //! Translate our Struct to the Core Data Entity
+    func mapToCoreDataObject(inContext managedContext: NSManagedObjectContext) -> NSManagedObject {
+        let entity =  NSEntityDescription.entityForName(coreDataEntityName,
+            inManagedObjectContext:
+            managedContext)
+
+        let managedObject = NSManagedObject(entity: entity!, insertIntoManagedObjectContext:managedContext)
+
+        managedObject.setValue(rackId, forKey: "rackId")
+        managedObject.setValue(lat, forKey:"lat")
+        managedObject.setValue(lng, forKey:"lng")
+        managedObject.setValue(name, forKey:"name")
+        managedObject.setValue(maxBikes, forKey:"maxBikes")
+        managedObject.setValue(availBikes, forKey:"availBikes")
+
+        return managedObject
+    }
 }
