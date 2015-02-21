431fe1c921e1c2891d2f8c61e5d1abe4df5ccf24
obusek <obusek@gmail.com>
2015-02-21 13:19:04 -0500

    Add the MapMarkerView to show when tapping a marker

---
+++ PhillyBikeShare/MapMarkerView.m
@@ -0,0 +1,13 @@
+//
+//  MapMarkerView.m
+//  PhillyBikeShare
+//
+//  Created by Andrew Obusek on 2/21/15.
+//  Copyright (c) 2015 Team Awesome. All rights reserved.
+//
+
+#import "MapMarkerView.h"
+
+@implementation MapMarkerView
+
+@end
