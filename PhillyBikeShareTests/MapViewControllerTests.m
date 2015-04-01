7db01bd748a49c95abee16801ba6ee6451e8773d
Andy Obusek <andyo@aweber.com>
2015-03-31 21:04:02 -0400

    Update google maps, and create test target

---
+++ PhillyBikeShareTests/MapViewControllerTests.m
@@ -0,0 +1,19 @@
+//
+//  MapViewControllerTests.m
+//  PhillyBikeShare
+//
+//  Created by Andy Obusek on 3/31/15.
+//  Copyright (c) 2015 Team Awesome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface MapViewControllerTests : XCTestCase
+
+@end
+
+@implementation MapViewControllerTests
+
+
+@end
